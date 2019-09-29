32a3cf52eaac8d8ec72f9fc7dc868cb92b3c7468
Asbjørn Madsen <asbjoern1982@gmail.com>
2019-09-29 09:21:15 +0200

    initial javafx commit

---
+++ java/src/dk/ninjabear/cipher/gui/Main.java
@@ -0,0 +1,15 @@
+package dk.ninjabear.cipher.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	public static void main(String[] args) {launch(args);}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Cipher");
+		primaryStage.show();
+	}
+
+}
---
+++ java/src/module-info.java
@@ -0,0 +1,7 @@
+module Cipher {
+	requires transitive javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	exports dk.ninjabear.cipher.gui;
+}
